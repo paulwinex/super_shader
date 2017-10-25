eb8a9903328ed3378f5e96240165ffef464bcb6e
paulwinex <tekken3>
2017-10-25 09:15:59 +0300

    add comment to shelf_button

--- shelf_button.py
+++ shelf_button.py
@@ -1,6 +1,11 @@
 import shader_converter
 reload(shader_converter)
 
+"""
+LieftClick - create new shader, copy parameters and replace references
+Ctrl + LeftCLick - Just Create and Copy parameters (for tests)
+"""
+
 if kwargs['ctrlclick']:
     # just convert
     shader_converter.convert()
